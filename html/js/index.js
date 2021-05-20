982d532b8f75eed140f6197ad1ca3f29a7eff9aa
Anny <anny@jk2u.com.tw>
2021-05-20 17:59:11 +0800

    anchor up

--- html/js/index.js
+++ html/js/index.js
@@ -6,6 +6,15 @@ $(function(){
         $("body,html").animate({scrollTop: $("#songprize").offset().top - 80},500);
     });
     
+    
+    $('a.goprize').click(function(){
+		$('html, body').animate({
+			scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top -80
+		}, 500);
+		return false;
+	});
+    
+    
 })
 
 
